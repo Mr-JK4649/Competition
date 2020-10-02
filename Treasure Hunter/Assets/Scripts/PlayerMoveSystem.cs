b06008bd07c33b1477c77d90feb815565589943a
Mr-JK4649 <ryouga>
2020-10-02 20:57:01 +0900

    移動のやつ修正

--- Treasure Hunter/Assets/Scripts/PlayerMoveSystem.cs
+++ Treasure Hunter/Assets/Scripts/PlayerMoveSystem.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class PlayerAutoRun : MonoBehaviour
+public class PlayerMoveSystem : MonoBehaviour
 {
 
-    public float RunSpeed = 0f;
+    public float RunSpeed = 0f;     //プレイヤーの自動飛行の速度
     public GameObject Wiz;          //プレイヤーオブジェクトを入れる
     public Transform Wiz_TF;        //プレイヤーのトランスフォーム
     public Rigidbody Wiz_RB;        //プレイヤーのRigitbody
 
+    public float LaneMoveSpeed = 0f;
+
     private void Start()
     {
-        Wiz = GameObject.Find("Wizard");
+        Wiz = this.gameObject;
         Wiz_TF = Wiz.GetComponent<Transform>();
         Wiz_RB = Wiz.GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        Wiz_RB.AddForce(new Vector3(0f, 0f, RunSpeed), ForceMode.Impulse);
-        Debug.DrawRay(Wiz_TF.position, new Vector3(Wiz_TF.position.x, -50f, Wiz_TF.position.z),new Color(255f,0f,0f));
         
+        float X_Move = Input.GetAxis("Horizontal") * LaneMoveSpeed;
+        float Y_Move = Input.GetAxis("Vertical") * LaneMoveSpeed;
+        //Wiz_RB.AddForce(new Vector3(X_Move, Y_Move, RunSpeed), ForceMode.Impulse);
+        Wiz_RB.velocity = new Vector3(X_Move, Y_Move, RunSpeed);
+
     }
+
+    
 }

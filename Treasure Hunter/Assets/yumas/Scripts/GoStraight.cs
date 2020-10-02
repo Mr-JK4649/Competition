85a941b263ea7feea1f69ecf49d48948ce661b9b
yumayuma48 <puxipu939@gmail.com>
2020-10-02 19:51:12 +0900

    渦に入ると加速する仕組みを作成

---
+++ Treasure Hunter/Assets/yumas/Scripts/GoStraight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoStraight : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, 100f), ForceMode.Impulse);
+        }
+    }
+}

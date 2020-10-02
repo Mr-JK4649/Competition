cb0ffffc997a9162faaf0a36428ebc82c4cfb97d
Mr-JK4649 <ryouga>
2020-10-02 21:14:01 +0900

    なにこれぇ！！？

--- Treasure Hunter/Assets/Scripts/PlayerMoveSystem.cs
+++ Treasure Hunter/Assets/Scripts/PlayerMoveSystem.cs
@@ -12,22 +12,26 @@ public class PlayerMoveSystem : MonoBehaviour
     public Transform Wiz_TF;        //プレイヤーのトランスフォーム
     public Rigidbody Wiz_RB;        //プレイヤーのRigitbody
 
-    public float LaneMoveSpeed = 0f;
+    public float LaneMoveSpeed = 0f;    //プレイヤーのレーン移動速度
+    private Vector3 Origin_Pos;         //プレイヤーの初期値 
 
     private void Start()
     {
-        Wiz = this.gameObject;
-        Wiz_TF = Wiz.GetComponent<Transform>();
-        Wiz_RB = Wiz.GetComponent<Rigidbody>();
+        Wiz = this.gameObject;                      //プレイヤーオブジェクトを入れる
+        Wiz_TF = Wiz.GetComponent<Transform>();     //プレイヤーのTransFormを入れる
+        Wiz_RB = Wiz.GetComponent<Rigidbody>();     //プレイヤーのRigitBodyを入れる
+
+        Origin_Pos = Wiz_TF.position;               //プレイヤーの初期位置を設定
     }
 
     private void Update()
     {
         
+        //if(Wiz_TF.position.x - 0.5f > Origin_Pos.x - 4.5f || )
         float X_Move = Input.GetAxis("Horizontal") * LaneMoveSpeed;
         float Y_Move = Input.GetAxis("Vertical") * LaneMoveSpeed;
-        //Wiz_RB.AddForce(new Vector3(X_Move, Y_Move, RunSpeed), ForceMode.Impulse);
         Wiz_RB.velocity = new Vector3(X_Move, Y_Move, RunSpeed);
+            
 
     }
 

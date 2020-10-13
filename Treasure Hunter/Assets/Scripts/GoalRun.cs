0c0d8d805d9fcf0c2a219c4d4fc7b4e89366745f
RyutoN0718 <ryuto-nakamoto-19h@stu.kbc.ac.jp>
2020-10-13 16:50:09 +0900

    ゴール関係script作成

---
+++ Treasure Hunter/Assets/Scripts/GoalRun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalRun : MonoBehaviour
+{
+    public PlayerMoveSystem PlayerMove;
+    float StartPlayer_z;
+
+    void Start()
+    {
+        StartPlayer_z = GameObject.Find("Wizard").transform.position.z;
+    }
+
+    void Update()
+    {
+        float Player_z = GameObject.Find("Wizard").transform.position.z;
+        this.GetComponent<Rigidbody>().velocity = PlayerMove.GetPlayerVelocity();
+    }
+}

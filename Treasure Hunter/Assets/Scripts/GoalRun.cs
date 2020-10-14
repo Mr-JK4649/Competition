a26e145f207feb27c850a7d11ad84695a24953de
RyutoN0718 <ryuto-nakamoto-19h@stu.kbc.ac.jp>
2020-10-14 22:24:44 +0900

    ゴール作成

--- Treasure Hunter/Assets/Scripts/GoalRun.cs
+++ Treasure Hunter/Assets/Scripts/GoalRun.cs
@@ -5,16 +5,44 @@ using UnityEngine;
 public class GoalRun : MonoBehaviour
 {
     public PlayerMoveSystem PlayerMove;
+    public GameObject stopObject;
+    public bool goalStop = false;
+    private Vector3 point;
     float StartPlayer_z;
 
     void Start()
     {
         StartPlayer_z = GameObject.Find("Wizard").transform.position.z;
+        point = stopObject.GetComponent<Transform>().position;
     }
 
     void Update()
     {
         float Player_z = GameObject.Find("Wizard").transform.position.z;
-        this.GetComponent<Rigidbody>().velocity = PlayerMove.GetPlayerVelocity();
+        if (goalStop == false)
+        {
+            this.GetComponent<Rigidbody>().velocity = PlayerMove.GetPlayerVelocity();
+        }
+        else
+        {
+            this.GetComponent<Transform>().position = new Vector3(point.x, this.GetComponent<Transform>().position.y, point.z);
+        }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "GoalStop")
+        {
+            goalStop = true;
+            Debug.Log("ヒット");
+            //this.enabled = false;
+        }
+    }
+    /*private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "GoalStop")
+        {
+            //goalStop = true;
+            this.enabled = false;
+        }
+    }*/
 }

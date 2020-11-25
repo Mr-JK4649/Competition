c2ac4d328ecd33cc84e8ac77ba057243fb20c9fc
yumayuma48 <puxipu939@gmail.com>
2020-11-25 10:48:52 +0900

    障害物透明化できるように

---
+++ Treasure Hunter/Assets/Scripts/Main/FadeIn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Block")
+        {
+
+            var color = other.GetComponent<Renderer>().material.color;
+
+            color.a = 1.0f;
+
+            other.GetComponent<Renderer>().material.color = color;
+        }
+    }
+}
--- Treasure Hunter/Assets/Scripts/Main/FadeOut.cs
+++ Treasure Hunter/Assets/Scripts/Main/FadeOut.cs
@@ -6,46 +6,59 @@ using UnityEngine.Scripting;
 public class FadeOut : MonoBehaviour
 {
 
-    public float startDistance = 70;
-    private float hiddenDistance = 25;
-    private GameObject v_camera;
-
-    private int coolTime = 90;
-    private int time = 0;
-
-    private void Start()
+    void OnTriggerExit(Collider other)
     {
-        v_camera = GameObject.Find("Wizard");
-    }
-
-    private void FixedUpdate()
-    {
-        //V_cameraとオブジェクトの距離を変数dに入れる
-        var d = Vector3.Distance(v_camera.transform.position, transform.position);
-
-        //透明度をいじるための変数格納
-        var color = this.GetComponent<Renderer>().material.color;
-
-        if (--time <= 0)
+        if (other.gameObject.tag == "Block")
         {
-            //ある程度距離が縮まったら完全透明化
-            if (d <= hiddenDistance)
-            {
-                Debug.Log("穂高！");
-                color.a = 0.2f;
-                time = coolTime;
-            }
-            else if (d <= startDistance)        //距離が一定以下になったら透明化開始
-            {
-                color.a = (d - hiddenDistance) / (startDistance - hiddenDistance) + 0.2f;
-            }
-            else                                //透明ではない
-            {
-                color.a = 1.0f;
-            }
-        }
+            var color = other.GetComponent<Renderer>().material.color;
 
-        //変更した処理を反映する
-        this.GetComponent<Renderer>().material.color = color;
+            color.a = 0.2f;
+
+            other.GetComponent<Renderer>().material.color = color;
+        }
     }
+
+    
+    //public float startDistance = 70;
+    //private float hiddenDistance = 25;
+    //private GameObject v_camera;
+
+    //private int coolTime = 90;
+    //private int time = 0;
+
+    //private void Start()
+    //{
+    //    v_camera = GameObject.Find("Wizard");
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    //V_cameraとオブジェクトの距離を変数dに入れる
+    //    var d = Vector3.Distance(v_camera.transform.position, transform.position);
+
+    //    //透明度をいじるための変数格納
+    //    var color = this.GetComponent<Renderer>().material.color;
+
+    //    if (--time <= 0)
+    //    {
+    //        //ある程度距離が縮まったら完全透明化
+    //        if (d <= hiddenDistance)
+    //        {
+    //            Debug.Log("穂高！");
+    //            color.a = 0.2f;
+    //            time = coolTime;
+    //        }
+    //        else if (d <= startDistance)        //距離が一定以下になったら透明化開始
+    //        {
+    //            color.a = (d - hiddenDistance) / (startDistance - hiddenDistance) + 0.2f;
+    //        }
+    //        else                                //透明ではない
+    //        {
+    //            color.a = 1.0f;
+    //        }
+    //    }
+
+    //    //変更した処理を反映する
+    //    this.GetComponent<Renderer>().material.color = color;
+    //}
 }

4a466cf8c505d59edc24b153ea20ccebb310f75e
Mr-JK4649 <ryouga>
2020-12-09 00:21:17 +0900

    a

--- Treasure Hunter/Assets/Animation/BoxOpenAnim/TreasureBoxAnim.cs
+++ Treasure Hunter/Assets/Animation/BoxOpenAnim/TreasureBoxAnim.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class TreasureBoxAnim : MonoBehaviour
 {
     public Animator anim;
+    public PlayerMoveSystem plms;
+    public PostProcessVolume volume;
+
 
     //private void OnCollisionEnter(Collision other)
     //{
@@ -18,6 +22,31 @@ public class TreasureBoxAnim : MonoBehaviour
         {
             anim.SetBool("Open", true);
 
+            plms.runSpd /= 20;
+
+        }
+
+        
+    }
+
+    [SerializeField] float inten = 0;
+    [SerializeField] float add = 0;
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+
+        if (other.gameObject.tag == "Player")
+        {
+            Bloom bl = ScriptableObject.CreateInstance<Bloom>();
+            bl.enabled.Override(true);
+            bl.intensity.Override(inten);
+
+
+            plms.runSpd /= 5;
+
+            volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 0f, bl);
         }
     }
+    
 }

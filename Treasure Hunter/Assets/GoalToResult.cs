da617f1c07f35c1f9f615f5ee875f9edc38f8e47
Mr-JK4649 <ryouga>
2020-12-10 17:08:34 +0900

    ｓ

---
+++ Treasure Hunter/Assets/GoalToResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalToResult : MonoBehaviour
+{
+    public void ToResult() { 
+        //GameObject.Find("GameSystem").GetComponent<GameManager>().
+    }
+    
+}

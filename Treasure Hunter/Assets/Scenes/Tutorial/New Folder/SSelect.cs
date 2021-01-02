a82ca6901a440489d1eeac1926ff59420c0e92a5
Mr-JK4649 <ryouga>
2021-01-02 21:25:53 +0900

    おけ

--- Treasure Hunter/Assets/Scenes/Tutorial/New Folder/SSelect.cs
+++ Treasure Hunter/Assets/Scenes/Tutorial/New Folder/SSelect.cs
@@ -8,17 +8,19 @@ public class SSelect : MonoBehaviour
     [SerializeField] private Text[] image;
     [SerializeField] private Image[] stg;
 
+    [SerializeField] private string[] name = new string[3];
+
     private void Update()
     {
         string th = this.GetComponent<Text>().text;
 
-        if (image[0].text == th) stg[0].enabled = true;
+        if (th == name[0]) stg[0].enabled = true;
         else stg[0].enabled = false;
 
-        if (image[1].text == th) stg[1].enabled = true;
+        if (th == name[1]) stg[1].enabled = true;
         else stg[1].enabled = false;
 
-        if (image[2].text == th) stg[2].enabled = true;
+        if (th == name[2]) stg[2].enabled = true;
         else stg[2].enabled = false;
     }
 }

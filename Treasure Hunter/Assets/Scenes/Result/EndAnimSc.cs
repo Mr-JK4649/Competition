0d9046209834acdc11a835e046444f2fd2e4d6fc
akado_ran <nisihuni1234@gmail.com>
2021-01-12 17:39:29 +0900

    Resultのアニメーションを追加したたたたったたたったた

---
+++ Treasure Hunter/Assets/Scenes/Result/EndAnimSc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndAnimSc : MonoBehaviour
+{
+    [SerializeField] private Resultscripts rs;
+    [SerializeField] Text tx;
+    [SerializeField] Slider clearPer;
+
+    void func() {
+        rs.EndAnimFunc();
+    }
+
+    void func2_1_tr() {
+        tx.enabled = true;
+        float num = rs.coinScore + rs.coinScore * clearPer.value * 100;
+        tx.text = num.ToString("00000000");
+    }
+
+    void func2_2() { 
+        rs.StartCoroutine("ClearStageEvaluation");
+    }
+}
